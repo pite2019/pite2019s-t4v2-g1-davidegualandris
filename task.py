5d620136dc266ccad460d09a6208056212df03a5
Davide Gualandris <davide.gualandris969@gmail.com>
2019-04-12 12:50:18 +0200

    first commit

--- task.py
+++ task.py
@@ -1,5 +1,5 @@
 #
-#Flight simulator. 
+#Flight simulator.
 #Write a code in python that simulates the tilt correction of the plane (angle between plane wings and earth). 
 #The program should print out current orientation, and applied tilt correction. 
 #The program should run in infinite loop, until user breaks the loop. 
@@ -13,8 +13,28 @@
 #Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
 #The goal of this task is for you to SHOW YOUR BEST python programming skills.
 #Impress everyone with your skills, show off with your code.
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+
+from random import randint
+
+class FlightSimulator:
+
+#Defining the constructor
+    
+    def __init__(self):
+        self.orientation = randint(0, 90)
+        self.disturb = randint(0, 7)
+
+    def adjust(self):
+        return randint(self.orientation-self.disturb, self.orientation+self.disturb)
+
+    def __str__(self):
+        return "It was " + str(self.orientation) + " angle, with " + str(self.disturb) + " disturbing. After adjusting it is " + str(self.adjust())
+
+if __name__ == '__main__':
+
+    while True:
+        choice = input ("Stop? [y]es, any keys for no").strip()
+        if choice == 'y':
+            break
+        sim = FlightSimulator()
+        print(sim)
