12a7d50c275b824d6e616f6e36a8524f4d8aae2a
Davide Gualandris <davide.gualandris969@gmail.com>
2019-04-24 11:27:01 +0200

    simulator

--- task.py
+++ task.py
@@ -1,40 +1,42 @@
-#
-#Flight simulator.
-#Write a code in python that simulates the tilt correction of the plane (angle between plane wings and earth). 
-#The program should print out current orientation, and applied tilt correction. 
-#The program should run in infinite loop, until user breaks the loop. 
-#Assume that plane orientation in every new simulation step is random angle with gaussian distribution (the planes is experiencing "turbulations"). 
-
-#With every simulation step the orentation should be corrected, applied and printed out.
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-
 from random import randint
+
 
 class FlightSimulator:
 
-#Defining the constructor
-    
+    # defining the constructor
     def __init__(self):
-        self.orientation = randint(0, 90)
-        self.disturb = randint(0, 7)
+        self.orientation = randint(-90, 90)  # orientation of the plane, the goal is to reach 0
 
+    # we are considering that the pilot adjust the plane and then a turbolence arrive\
+    # basically he is not expecting the turbolence
     def adjust(self):
-        return randint(self.orientation-self.disturb, self.orientation+self.disturb)
+
+        # at every step, the angle could be adjusted of maximum 10 degrees by the pilot
+        if -10 <= self.orientation <= 10:
+            self.orientation = 0
+        else:
+            if self.orientation < 0:
+                self.orientation += 10
+            else:
+                self.orientation -= 10
+
+        # the turbolence is between -10 and 10 degrees
+        turb = randint(-10, 10)
+        self.orientation += turb
+
+        return self.orientation, turb
 
     def __str__(self):
-        return "It was " + str(self.orientation) + " angle, with " + str(self.disturb) + " disturbing. After adjusting it is " + str(self.adjust())
+        return "The angle is " + str(self.orientation)
+
 
 if __name__ == '__main__':
 
+    fs = FlightSimulator()
+    print(fs)
+
     while True:
-        choice = input ("Stop? [y]es, any keys for no").strip()
-        if choice == 'y':
+        if input("Stop? [y]es, any other keys for no").strip() == 'y':
             break
-        sim = FlightSimulator()
-        print(sim)
+        res = fs.adjust()
+        print("After the adjustment, the angle is " + str(res[0]) + ", the turbolence was " + str(res[1]))
