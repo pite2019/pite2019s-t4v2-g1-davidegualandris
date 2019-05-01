defb38db9ad5e72c93694f9b96f9eec6767473dc
Davide Gualandris <davide.gualandris969@gmail.com>
2019-05-01 10:00:53 +0200

    Improvment

--- task.py
+++ task.py
@@ -1,42 +1,73 @@
-from random import randint
+from random import gauss
+import logging
+import time
 
 
-class FlightSimulator:
+# class for handle the Environment
+class Environment:
+
+    # defining the constructor
+    def __init__(self, plane):
+        # TODO: more than one plane in the environment
+        self.plane = plane  # the environment contains the plane
+
+    # generator to use the simulate one step
+    # we are considering that the pilot adjust the plane and then a turbolence arrive\
+    # basically he is not expecting the turbolence
+    def next_simulation(self):
+        while True:
+            self.plane.adjust()
+            turb = gauss(0, 4)
+            self.plane.orientation += turb
+            if -180 < self.plane.orientation < 180:
+                yield self.plane.orientation, turb
+                continue
+            break
+
+    # function to print the environment
+    def __str__(self):
+        return self.plane.__str__()
+
+
+# class for handle the Plane
+# the orientation of the plane
+class Plane:
 
     # defining the constructor
     def __init__(self):
-        self.orientation = randint(-90, 90)  # orientation of the plane, the goal is to reach 0
+        self.orientation = gauss(0, 13.4164)  # orientation of the plane is between -180 and 180
 
-    # we are considering that the pilot adjust the plane and then a turbolence arrive\
-    # basically he is not expecting the turbolence
+    # the pilot is trying to adjust the airplane
     def adjust(self):
-
-        # at every step, the angle could be adjusted of maximum 10 degrees by the pilot
-        if -10 <= self.orientation <= 10:
+        # at every step, the angle could be adjusted of maximum 3 degrees by the pilot
+        # because the sigma for the disturbance is 4
+        if -3 <= self.orientation <= 3:
             self.orientation = 0
         else:
             if self.orientation < 0:
-                self.orientation += 10
+                self.orientation += 3
             else:
-                self.orientation -= 10
+                self.orientation -= 3
 
-        # the turbolence is between -10 and 10 degrees
-        turb = randint(-10, 10)
-        self.orientation += turb
-
-        return self.orientation, turb
-
+    # function to print the environment
     def __str__(self):
         return "The angle is " + str(self.orientation)
 
 
 if __name__ == '__main__':
 
-    fs = FlightSimulator()
-    print(fs)
+    # create the plane
+    pln = Plane()
 
-    while True:
-        if input("Stop? [y]es, any other keys for no").strip() == 'y':
-            break
-        res = fs.adjust()
-        print("After the adjustment, the angle is " + str(res[0]) + ", the turbolence was " + str(res[1]))
+    # create the environment
+    env = Environment(pln)
+
+    logging.basicConfig(filename='simulation.log', level=logging.DEBUG)
+
+    logging.info(env)
+
+    # iterate the simulations
+    for sim in env.next_simulation():
+        logging.info("After the adjustment, the angle is " + str(sim[0]) + ", the turbolence was " + str(sim[1]))
+        time.sleep(2)
+    logging.error("Oooooh no, the plane has crashed \n")
